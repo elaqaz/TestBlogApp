ca322aeb2333b551bb8fb0033b72ef03f63f5f31
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-04-30 12:10:37 +0100

    add validate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+    validates :content, presence: true, length: { minimum: 20, maximum: 10000 }  
 end
